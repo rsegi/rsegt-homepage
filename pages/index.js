6337b5aafd4368276462587ecad339fa0df9d11c
Raul Segui <r.segui.t@gmail.com>
2021-12-13 18:03:17 +0100

    create main page with section

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,30 @@
+import { Box, Container, Heading } from '@chakra-ui/layout'
+import Section from '../components/section'
+
 const Page = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
-        </div>
+        <Container>
+            <Box borderRadius="lg" mb={8} p={3}>
+                <h1>
+                    Hello I&apos;m a full-stack developer based in sunny
+                    Barcelona.
+                </h1>
+            </Box>
+            <Box display={{ md: 'flex' }}>
+                <Box flexGrow={1}>
+                    <Heading as="h2" variant="page-title">
+                        Raúl Seguí
+                    </Heading>
+                    <p>Developer - Skateboarder/Snowboarder - Food enjoyer</p>
+                </Box>
+            </Box>
+            <Section delay={0.1}>
+                <Heading as="h3" variant="section-title">
+                    Work
+                </Heading>
+                <p>Paragraph</p>
+            </Section>
+        </Container>
     )
 }
 
